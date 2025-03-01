56ddf62cbb4682325eccbd11dc278d19fdead7f4
shakirthesenior@gmail.com <shakirthesenior@gmail.com>
2025-03-01 14:21:23 -0500

    adding GameLogic script to capture global game var

--- Scripts/GameLogic.cs
+++ Scripts/GameLogic.cs
@@ -1,4 +1,14 @@
 using Godot;
 using System;
 
+public partial class GameLogic : Node
+{
+public override void _Ready()
+{
+    var player1AS = GetNode<player1>("player1").attentionSpan;
+    var player2AS = GetNode<player2>("player2").attentionSpan;
+    var player3AS = GetNode<player3>("player3").attentionSpan;
 
+}
+
+}
--- Scripts/SessionOverScreen.cs
+++ Scripts/SessionOverScreen.cs
@@ -10,6 +10,10 @@ public partial class SessionOverScreen : Control
 		var button = GetNode<Button>("Button");
 		button.Text = "Retry?";
 		button.Pressed += Retry;
+
+		var playerAvgASLabel = GetNode<Label>("playerAvgASLabel");
+		var GameLogic = GetNode<GameLogic>("GameLogicVar");
+		playerAvgASLabel.Text = "Avg. Player Attention Span:"+ GameLogic;
 	}
 
 	private void Retry()
